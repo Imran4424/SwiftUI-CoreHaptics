e70d2572de9e4a9fe8ff5d0f9ea6fdac041df827
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-11-06 21:41:43 +0600

    project init

---
+++ CoreHapticss/CoreHapticssApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreHapticssApp.swift
+//  CoreHapticss
+//
+//  Created by Shah Md Imran Hossain on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreHapticssApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
