b5bae2c294af49a1725497003176ab3d3a4e6498
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-11-06 21:47:56 +0600

    add home view

--- CoreHapticss/HomeView.swift
+++ CoreHapticss/HomeView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var index = 0
+    
+    let viewTitle = [
+        "UIKit Haptics",
+        "Core Haptics"
+    ]
+    
+    let viewList: [AnyView] = [
+        AnyView(UIKitHapticsView()),
+        AnyView(CoreHapticsView())
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(viewTitle[index])
+            Spacer()
+            
+            viewList[index]
+            
+            Spacer()
+            
+            HStack {
+                if index != 0 {
+                    Button("Previous") {
+                        if index > 0 {
+                            index = index - 1
+                        }
+                    }
+                }
+                
+                
+                Spacer()
+                
+                if index != viewList.count - 1 {
+                    Button("Next") {
+                        if index < viewList.count - 1 {
+                            index = index + 1
+                        }
+                    }
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
